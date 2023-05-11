dd619a35d7a1e7fbc86489145bc775834a0a28d0
Stas <puzanov05@gmail.com>
2023-05-12 00:31:02 +0300

    Correction

--- Puzanov_Lesson_14.py
+++ Puzanov_Lesson_14.py
@@ -1,37 +1,37 @@
 import sys
-while True:
-    def calculator(a, b):
-        def operation(c):
-            if c == '0':
-                def ex():
-                     sys.exit(0)
-                ex()
-            elif c == '/':
-                def div():
-                    try:
-                        return f'The  result of division is {a / b}'
-                    except ZeroDivisionError:
-                        return "You can't divide by zero!"
-                print(div())
-            elif c == '*':
-                def mult():
-                    return f'The result of multiplication is {a * b}'
-                print(mult())
-            elif c == '+':
-                def sm():
-                    return f'The result of summation is {a + b}'
-                print(sm())
-            elif c == '-':
-                def subtr():
-                    return f'The result of subtraction is {a - b}'
-                print(subtr())
-            else:
-                def idk():
-                    return 'unknown operation'
-                print(idk())
-            return ''
-        print(operation(input('Enter an operation(0 to exit the calculator, /, *, +, -): ')))
+
+def calculator(a, b):
+    def operation(c):
+        def ex():
+            sys.exit(0)
+        def div():
+            try:
+                return f'The  result of division is {a / b}'
+            except ZeroDivisionError:
+                return "You can't divide by zero!"
+        def mult():
+            return f'The result of multiplication is {a * b}'
+        def sm():
+            return f'The result of summation is {a + b}'
+        def subtr():
+            return f'The result of subtraction is {a - b}'
+        def idk():
+            return 'unknown operation'
+        if c == '0':
+            print(ex())
+        elif c == '/':
+            print(div())
+        elif c == '*':
+            print(mult())
+        elif c == '+':
+            print(sm())
+        elif c == '-':
+            print(subtr())
+        else:
+            print('Unknown operation!')
         return ''
+    return operation(input('Enter an operation(0 to exit, /, *, +, -): '))
+while True:
     try:
         print(calculator(int(input('Enter the first number: ')), int(input('Enter the second number: '))))
     except ValueError:
