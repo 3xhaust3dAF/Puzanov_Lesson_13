2dae079e230bdcda2ae9c908134f161a6550da5e
Stas <puzanov05@gmail.com>
2023-05-15 23:09:17 +0300

    Сделано первое

--- Puzanov_Lesson_15.py
+++ Puzanov_Lesson_15.py
@@ -1,5 +1,6 @@
 #1
 def f_info(var):
+    global j
     nms = 0
     chs = 0
     lngth = 0
@@ -11,27 +12,34 @@ def f_info(var):
                 continue
             elif str(i).isalpha():
                 lngth += len(i)
-        return f' {lngth} символа(ов)'
+        print(f' {lngth} символа(ов)')
     elif type(var) == list:
         for i in var:
-            for j in var[str(i).index(var)]:
-                if str(j).isdigit():
+            if type(i) == str:
+                for j in var[var.index(str(i))]:
+                    if str(j).isdigit():
+                        nms += 1
+                    elif str(j).isalpha():
+                        chs += 1
+            else:
+                if str(i).isdigit():
                     nms += 1
-                elif str(j).isalpha():
+                elif str(i).isalpha():
                     chs += 1
-        return f'{nms} числа, {chs} буквы'
+        print(f'{nms} числа, {chs} буквы')
     elif type(var) == int:
         for o in var:
             if o % 2 != 0:
                 odd += 1
-        return f'{odd} нечётных цифр'
+        print(f'{odd} нечётных цифр')
     elif type(var) == str:
         for p in var:
             if str(p).isalpha():
                 chs_1 += 1
-        return f'{chs_1} - кол-во букв'
+        print(f'{chs_1} - кол-во букв')
     else:
         raise TypeError('Такой тип не поддерживается')
 
-f_info('123asd')
+
+f_info([1, 2, 3, 'a', 'bc8?'])
 
